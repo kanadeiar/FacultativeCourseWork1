c7cec05f599d5faf66bab83c09b9d034259d0bff
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-30 19:54:12 +0300

    Добавление главного меню курсовой

--- FacultativeCourseWork1/Program.cs
+++ FacultativeCourseWork1/Program.cs
@@ -10,6 +10,23 @@ namespace FacultativeCourseWork1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*** Наша курсовая работа ***");
+            while (true)
+            {
+                Console.WriteLine("Выберите домашнюю работу для показа (q-выход):");
+                string line = Console.ReadLine();
+                if (String.Equals(line,"q"))
+                    break;
+                int.TryParse(line, out int numberHomeWork);
+                switch (numberHomeWork)
+                {
+                    default:
+                        Console.WriteLine("Нет домашней работы под таким номером!");
+                        break;
+                }
+            }
+            Console.WriteLine("Нажмите любую кнопку для выхода ...");
+            Console.ReadKey();
         }
     }
 }
