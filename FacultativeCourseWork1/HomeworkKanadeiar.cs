ed70f5b8815c150038bc3370366ca9e00a77f4b0
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-30 20:43:12 +0300

    Добавлена ДЗ Рассахатского. Все работает

---
+++ FacultativeCourseWork1/HomeworkKanadeiar.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FacultativeCourseWork1
+{
+    class HomeworkKanadeiar
+    {
+        public static void Demo()
+        {
+            WriteLine("Данное домашнее задание выполнил: Рассахатский");
+
+            string surname = getStringFromConsole("Введите фамилию человека");
+            string name = getStringFromConsole("Введите имя человека");
+            int age = getIntFromConsole("Введите возраст человека");
+            int growth = getIntFromConsole("Введите рост человека (см)");
+            int weight = getIntFromConsole("Введите вес человека (кг)");
+            WriteLine("\n\nВывод информации:\n");
+            WriteLine("Используя склеивание:");
+            WriteLine("Фамилия: " + surname + " Имя: " + name + " Возраст: " + age + " лет Рост: " + growth + " см Вес: " + weight + " кг.");
+            WriteLine("\nИспользуя форматированный вывод:");
+            WriteLine("Фамилия: {0} Имя: {1} Возраст: {2} лет Рост: {3} см Вес: {4} кг.", surname, name, age, growth, weight);
+            WriteLine("\nИспользуя вывод со знаком $:");
+            WriteLine($"Фамилия: {surname} Имя: {name} Возраст: {age} лет Рост: {growth} см Вес: {weight} кг.");
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Получение строки с консоли
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static string getStringFromConsole(string message)
+        {
+            Write($"{message}:>");
+            return ReadLine();
+        }
+        /// <summary>
+        /// Получение числа с консоли
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static int getIntFromConsole(string message)
+        {
+            while (true)
+            {
+                Write($"{message}:>");
+                if (int.TryParse(ReadLine(), out int number))
+                {
+                    return number;
+                }
+                WriteLine("Ошибка! Введен неверный формат целого числа!");
+                Beep(500,500);
+            }
+        }
+
+
+    }
+}
--- FacultativeCourseWork1/Program.cs
+++ FacultativeCourseWork1/Program.cs
@@ -14,12 +14,17 @@ namespace FacultativeCourseWork1
             while (true)
             {
                 Console.WriteLine("Выберите домашнюю работу для показа (q-выход):");
+                Console.WriteLine("1 - Домашняя работа \"Написать программу Анкета\"" +
+                                  " (выполнил: Рассахатский)");
                 string line = Console.ReadLine();
                 if (String.Equals(line,"q"))
                     break;
                 int.TryParse(line, out int numberHomeWork);
                 switch (numberHomeWork)
                 {
+                    case 1:
+                        HomeworkKanadeiar.Demo();
+                        break;
                     default:
                         Console.WriteLine("Нет домашней работы под таким номером!");
                         break;
