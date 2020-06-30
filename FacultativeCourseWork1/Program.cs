c3a2735fa0b7237d1e8a05c33db0df2a9bc74123
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2020-06-30 20:36:21 +0300

    Удалил лишний мусор

--- FacultativeCourseWork1/Program.cs
+++ FacultativeCourseWork1/Program.cs
@@ -10,21 +10,16 @@ namespace FacultativeCourseWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** Наша курсовая работа ***");
+            Console.WriteLine("*** Наша пробная курсовая работа ***");
             while (true)
             {
                 Console.WriteLine("Выберите домашнюю работу для показа (q-выход):");
-                Console.WriteLine("1 - Домашняя работа \"Написать программу Анкета\"" +
-                                  " (выполнил: Тестов Тест Тестович)");
                 string line = Console.ReadLine();
                 if (String.Equals(line,"q"))
                     break;
                 int.TryParse(line, out int numberHomeWork);
                 switch (numberHomeWork)
                 {
-                    case 1:
-                        HomeworkTestov.Demo(); //демонстрация
-                        break;
                     default:
                         Console.WriteLine("Нет домашней работы под таким номером!");
                         break;
