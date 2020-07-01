c068dbb2da13e4beb46bb8c720c9c8599de192da
Gennadiy <gonzy@yandex.ru>
2020-07-01 14:47:42 +0400

    Тертычный. Добавил своё первое приложение на c#

--- FacultativeCourseWork1/Program.cs
+++ FacultativeCourseWork1/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FacultativeCourseWork1.Array_s;
+using System;
 
 namespace FacultativeCourseWork1
 {
@@ -10,28 +7,11 @@ namespace FacultativeCourseWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("*** Наша пробная курсовая работа ***");
-            while (true)
-            {
-                Console.WriteLine("Выберите домашнюю работу для показа (q-выход):");
-                Console.WriteLine("1 - Домашняя работа \"Написать программу Анкета\"" +
-                                  " (выполнил: Рассахатский)");
-                string line = Console.ReadLine();
-                if (String.Equals(line,"q"))
-                    break;
-                int.TryParse(line, out int numberHomeWork);
-                switch (numberHomeWork)
-                {
-                    case 1:
-                        HomeworkKanadeiar.Demo();
-                        break;
-                    default:
-                        Console.WriteLine("Нет домашней работы под таким номером!");
-                        break;
-                }
-            }
-            Console.WriteLine("Нажмите любую кнопку для выхода...");
-            Console.ReadKey();
+            MyBestArray.PrintNameProgramme();
+            MyBestArray.firstDimension = MyBestArray.ValidateNumberLine();
+            MyBestArray.secondDimension =MyBestArray.ValidateNumberColumn();
+            MyBestArray.arrayUser = MyBestArray.CreateArray(MyBestArray.arrayUser, MyBestArray.firstDimension, MyBestArray.secondDimension);
+            MyBestArray.ShowMenuV2();
         }
     }
 }
