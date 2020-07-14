e841679a2a9aea9f35f1c86beb299101835f749a
Alexey Kulik <kpblc2000@gmail.com>
2020-07-14 19:10:03 +0300

    Смена namespace

--- FacultativeCourseWork1/HomeWorkKulik/Lesson04Task5_TwoDimArrays/Program.cs
+++ FacultativeCourseWork1/HomeWorkKulik/Lesson04Task5_TwoDimArrays/Program.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task5
+namespace FacultativeCourseWork1.HomeWorkKulik
 {
 	class Program
 	{
--- FacultativeCourseWork1/HomeWorkKulik/Lesson04Task5_TwoDimArrays/TwoDimArray.cs
+++ FacultativeCourseWork1/HomeWorkKulik/Lesson04Task5_TwoDimArrays/TwoDimArray.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 
-namespace Task5
+namespace FacultativeCourseWork1.HomeWorkKulik
 {
 	class TwoDimArray
 	{
